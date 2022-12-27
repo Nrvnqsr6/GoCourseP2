418252aedb06abc5ee26e3d9c3ce55fd4c9de8a1
Riniker <zverekqw12@gmail.com>
2022-12-27 16:50:13 +0400

    final

--- internal/service/handler.go
+++ internal/service/handler.go
@@ -1,32 +1,32 @@
 package service
 
-import (
-	"net/http"
-	"part2/internal/adaptor"
-	"part2/internal/model"
+// import (
+// 	"net/http"
+// 	"part2/internal/adaptor"
+// 	"part2/internal/model"
 
-	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
-)
+// 	"github.com/gin-gonic/gin"
+// 	"github.com/google/uuid"
+// )
 
-type UserHandler struct {
-	storage *adaptor.ConcurrentUserStorage
-}
+// type UserHandler struct {
+// 	storage *adaptor.ConcurrentUserStorage
+// }
 
-func (handler *UserHandler) Get(ctx *gin.Context) {
-	user := handler.storage.Get(ctx.Param("login"))
-	ctx.IndentedJSON(http.StatusOK, user)
-}
+// func (handler *UserHandler) Get(ctx *gin.Context) {
+// 	user := handler.storage.Get(ctx.Param("login"))
+// 	ctx.IndentedJSON(http.StatusOK, user)
+// }
 
-func (handler *UserHandler) Put(ctx *gin.Context) {
-	newUser := model.User{ID: uuid.New()}
-	err := ctx.BindJSON(&newUser)
-	if err != nil {
-		return
-	}
-	handler.storage.Add(&newUser)
-	ctx.IndentedJSON(http.StatusOK, newUser)
-}
+// func (handler *UserHandler) Put(ctx *gin.Context) {
+// 	newUser := model.User{ID: uuid.New()}
+// 	err := ctx.BindJSON(&newUser)
+// 	if err != nil {
+// 		return
+// 	}
+// 	handler.storage.Add(&newUser)
+// 	ctx.IndentedJSON(http.StatusOK, newUser)
+// }
 
 // func (handler *UserHandler) Post(ctx *gin.Context) {
 // 	var newUser *model.User
